9740c002914e0f6fbb069f63b5174908c4f137c2
mojahid-belaman <mojahidbelaman@gmail.com>
2023-08-08 17:58:46 +0100

    create own animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        startIn: "fadeIn 1s ease-out forwards",
+      },
+    },
+    fontFamily: {
+      myFont: ["Inter", "Avenir", "Helvetica", "Arial", "sans-serif"],
+    },
+    keyframes: {
+      fadeIn: {
+        from: {
+          opacity: "0",
+          transform: "translateY(1rem)",
+        },
+        to: {
+          opacity: "1",
+          transform: "translateY(0)",
+        },
+      },
+    },
   },
   plugins: [],
 };
