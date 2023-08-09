fdb5929fc70690567f41ede1fc87bd667d8ee3e9
mojahid-belaman <mojahidbelaman@gmail.com>
2023-08-09 12:32:49 +0100

    change place the state toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+import MainHeader from "./components/MainHeader";
 import PostList from "./components/PostList";
 
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  const toggeleModal = () => {
+    setModalIsVisible((prev) => !prev);
+  };
   return (
-    <main className="min-h-screen bg-gradient-to-b from-purple-600">
-      <PostList />
-    </main>
+    <div className="min-h-screen bg-gradient-to-b from-purple-600">
+      <MainHeader toggele={toggeleModal} />
+      <main>
+        <PostList modalIsVisible={modalIsVisible} toggeleModal={toggeleModal}  />
+      </main>
+    </div>
   );
 }
 
