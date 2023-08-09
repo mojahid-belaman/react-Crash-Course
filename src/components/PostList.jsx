f044750db1a7b4420085bc839daf034c07b3cc04
mojahid-belaman <mojahidbelaman@gmail.com>
2023-08-09 18:05:32 +0100

    add post in state

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,30 +1,27 @@
-import React, { useState } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
+import { useState } from "react";
 
-const PostList = ({modalIsVisible, toggeleModal}) => {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  let modalContent;
-  if (modalIsVisible) {
-    modalContent = (
-      <Modal onToggleModal={toggeleModal}>
-        <NewPost
-          onBodyChange={setEnteredBody}
-          onAuthorChange={setEnteredAuthor}
-        />
-      </Modal>
-    );
-  }
+const PostList = ({ modalIsVisible, toggeleModal }) => {
+  const [posts, setPosts] = useState([]);
   return (
     <>
-      {modalContent}
-      <ul className="max-w-4xl mx-auto py-5 grid gap-3 grid-cols-3 justify-center">
-        <Post author={enteredAuthor} body={enteredBody} />
-        <Post author="Mojahid Belaman" body="React Js Crash Course." />
-      </ul>
+      {modalIsVisible && (
+        <Modal onToggleModal={toggeleModal}>
+          <NewPost onCancel={toggeleModal} onAddPost={setPosts} />
+        </Modal>
+      )}
+      {/* <Post author="Mojahid Belaman" body="React Js Crash Course." /> */}
+      {posts.length > 0 ? (
+        <ul className="py-5 grid gap-3 grid-cols-3 justify-center">
+          {posts.map((elem, idx) => (
+            <Post key={idx} author={elem.author} body={elem.body} />
+          ))}
+        </ul>
+      ) : (
+        <p className="text-center text-white text-2xl mt-5">There are no posts yet!</p>
+      )}
     </>
   );
 };
