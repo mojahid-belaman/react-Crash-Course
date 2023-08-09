41c81cb804da98a56e8b684c38e0c37d8e077b7e
mojahid-belaman <mojahidbelaman@gmail.com>
2023-08-09 12:31:09 +0100

    add icon button

---
+++ src/icons/IconAddPost.jsx
@@ -0,0 +1,24 @@
+
+function IconAddPost(props) {
+  return (
+    <svg
+      viewBox="0 0 21 21"
+      fill="currentColor"
+      height="1.5em"
+      width="1.5em"
+      {...props}
+    >
+      <g
+        fill="none"
+        fillRule="evenodd"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M9.5 5.5h-4a2 2 0 00-2 2v7a2 2 0 002 2h7a2 2 0 002-2v-4M14.5 2.5v6M17.5 5.5h-6" />
+      </g>
+    </svg>
+  );
+}
+
+export default IconAddPost;
