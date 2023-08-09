5bdeca64c9bbd986fe702025cdf92df4151c8a6f
mojahid-belaman <mojahidbelaman@gmail.com>
2023-08-09 12:31:39 +0100

    change method rendering

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,24 +3,24 @@ import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 
-const PostList = () => {
+const PostList = ({modalIsVisible, toggeleModal}) => {
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
-  const [modalIsVisible, setModalIsVisible] = useState(true);
 
-  const toggleModal = () => {
-    setModalIsVisible((prev) => !prev);
-  };
+  let modalContent;
+  if (modalIsVisible) {
+    modalContent = (
+      <Modal onToggleModal={toggeleModal}>
+        <NewPost
+          onBodyChange={setEnteredBody}
+          onAuthorChange={setEnteredAuthor}
+        />
+      </Modal>
+    );
+  }
   return (
     <>
-      {modalIsVisible && (
-        <Modal onToggleModal={toggleModal}>
-          <NewPost
-            onBodyChange={setEnteredBody}
-            onAuthorChange={setEnteredAuthor}
-          />
-        </Modal>
-      )}
+      {modalContent}
       <ul className="max-w-4xl mx-auto py-5 grid gap-3 grid-cols-3 justify-center">
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Mojahid Belaman" body="React Js Crash Course." />
