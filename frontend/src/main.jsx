278d811229e2db76efcef0145e356a7172e8a13d
mojahid-belaman <mojahidbelaman@gmail.com>
2023-08-16 14:40:36 +0100

    add new pages like single post

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import NewPost, { action as newPostAction } from "./pages/NewPost.jsx";
 import RootLayout from "./pages/RootLayout.jsx";
 import Posts, { loader as loaderPost } from "./pages/Posts.jsx";
 import "./index.css";
+import EditPost, { loader as EditPostLoader } from "./pages/EditPost.jsx";
 
 const router = createBrowserRouter([
   {
@@ -17,7 +18,8 @@ const router = createBrowserRouter([
         element: <Posts />,
         loader: loaderPost,
         children: [
-          { path: "/newPost", element: <NewPost />, action: newPostAction },
+          { path: "newPost", element: <NewPost />, action: newPostAction },
+          { path: ":id", element: <EditPost />, loader: EditPostLoader },
         ],
       },
     ],
